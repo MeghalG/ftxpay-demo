e145189609be4e5ffca51276b23511af6df1f87a
Meghal2 Gupta <meghal2@Meghals-MacBook-Pro.local>
2022-09-12 18:03:48 -0400

    first material ui front end update

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+import Store from './pages/Store'
+import Checkout from './pages/Checkout'
+
+import { Button, Toolbar, Typography, Avatar, AppBar } from "@mui/material";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
 
 function App() {
+
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
@@ -11,12 +17,17 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
+  const [checkout, setCheckout] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4" sx={{ flexGrow: 1 }}>Candy Store</Typography>
+          <Button style={{ color: 'white' }}><ShoppingCartIcon /></Button>
+        </Toolbar>
+      </AppBar>
+      <Store />
     </div>
   );
 }
---
+++ client/src/components/Item.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+function Item() {
+    return (
+        <p>Item</p>
+    );
+}
+
+export default Item;
